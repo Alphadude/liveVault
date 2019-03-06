7323fdfd35cbb9770ca10c49bf6681a59cd6b03c
Alphadude <alphadude4all@gmail.com>
2019-03-06 17:15:46 +0100

    added share button and rate button

--- app/src/main/java/com/alphadude/user/bettingtipz/HomeScreen.java
+++ app/src/main/java/com/alphadude/user/bettingtipz/HomeScreen.java
@@ -1,6 +1,8 @@
 package com.alphadude.user.bettingtipz;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +15,7 @@ import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
 
-    private Button freetips, vipsub, viparchives, Rate;
+    private Button freetips, vipsub, viparchives;
     ActionBar actionBar = null;
 
     @Override
@@ -28,7 +30,7 @@ public class HomeScreen extends AppCompatActivity {
         freetips = findViewById(R.id.freetips);
         vipsub =  findViewById(R.id.vipsub);
         viparchives =  findViewById(R.id.viparchives);
-        Rate =  findViewById(R.id.rate);
+        //Rate =  findViewById(R.id.rate);
 
 
 
@@ -54,12 +56,12 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
 
-        Rate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Viparchives();
-            }
-        });
+//        Rate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                rateMe();
+//            }
+//        });
 
 
     }
@@ -77,6 +79,17 @@ public class HomeScreen extends AppCompatActivity {
     public void Viparchives(){
         Intent open = new Intent(this, VipArchives.class);
         startActivity(open);
+    }
+
+    public void rateMe(View v){
+        try{
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + "com.android.chrome")));
+        } catch (ActivityNotFoundException e){
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + "com.android.chrome")));
+        }
+        git
     }
 
     public void login(){
@@ -100,7 +113,14 @@ public class HomeScreen extends AppCompatActivity {
             case R.id.settings:
                 startActivity(new Intent(this, Settings.class));
                 return true;
-
+            case R.id.share:
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                String shareBody = "hello welcome to one slip 2+3 odds download from www.helixgade.com ";
+                //String shareTopic ="header to see what works";
+                share.putExtra(Intent.EXTRA_TEXT,shareBody);
+                //share.putExtra(Intent.EXTRA_SUBJECT,shareTopic);
+                startActivity(Intent.createChooser(share,"Share One Slip 2+3 Odds 100% win"));
             default:
                 return super.onOptionsItemSelected(item);
         }
--- app/src/main/java/com/alphadude/user/bettingtipz/Settings.java
+++ app/src/main/java/com/alphadude/user/bettingtipz/Settings.java
@@ -4,18 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import mehdi.sakout.fancybuttons.FancyButton;
 
 public class Settings extends AppCompatActivity {
 
-    FancyButton login ;
+
+    TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        login = (FancyButton)findViewById(R.id.btnSettingLogin);
+
+        login = findViewById(R.id.betwisely);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
